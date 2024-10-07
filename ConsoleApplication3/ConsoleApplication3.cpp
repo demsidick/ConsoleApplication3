5a3402cc8a37b385f90231d6cb6e509e593d84b6
User <User@DESKTOP-T59014S>
2024-10-07 21:12:15 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApplication3/ConsoleApplication3.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+using namespace std;
+int main() {
+
+
+
+    double meters, kilometers;
+
+    cout << "enter meters: ";
+    cin >> meters;
+
+    kilometers = meters / 1000.0;
+
+    cout << meters << " meters = " << kilometers << " kilometers" << endl;
+
+}
